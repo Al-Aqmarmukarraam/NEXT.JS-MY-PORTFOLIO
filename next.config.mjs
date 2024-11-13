fa652fb7910dc9cf023e1fdfdee4278478491c77
Al-Aqmarmukarraam <alaqmarmukarram786@gmail.com>
2024-11-13 20:30:47 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const NextConfig = {
+    eslint:
+    {ignoreDuringBuilds : true}
+ };
 
-export default nextConfig;
+ export default NextConfig;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+ import { RiMenuFold3Fill } from "react-icons/ri";
+
+const Navbar = () => {
+  return (
+    <div className='container pt-8'>
+      <div className='flex justify-between items-center'>
+      <div className='text-xl font-medium'>AL-AQMAR MUKARRAM</div>
+            <ul className='gap-10 lg:gap-16 hidden md:flex'>
+              <li className='menuLink'><a href='#Hero'>Home</a></li>
+              <li className='menuLink'><a href='#About'>About</a></li>
+              <li className='menuLink'><a href='#Projects'>Projects</a></li>
+              <li className='menuLink'><a href='#Skills'>Skills</a></li>
+              <li className='menuLink'><a href='#Contact'>Contact</a></li>
+            </ul>
+            <RiMenuFold3Fill size={30}/>
+            </div>
+        
+       </div>
+  )
+}
+export default Navbar
